a10266612652382c4ab21435ed4a156ce8e65d37
njdeane <njdeane7@gmail.com>
2020-11-11 17:04:58 +1000

    added ejs template with day of the week serverside code

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function (req, res) {
   var today = new Date();
   var currentDay = today.getDay();
+  var day = "";
 
-  if (currentDay === 6 || currentDay === 0) {
-    res.write("<h1>Enjoy your weekend</h1>");
-    }  else {
-      res.sendFile(__dirname + "/index.html");
-    }
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday"
+      break;
+    default:
+    console.log("Error: current day is equal too " + currentDay);
+  }
 
+  res.render("list", { kindOfDay: day });
 });
-
-
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
