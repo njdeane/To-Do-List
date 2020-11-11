67ec143d5facd34776514ce65694ba0e6dad54e2
njdeane <njdeane7@gmail.com>
2020-11-11 16:00:26 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  if (currentDay === 6 || currentDay === 0) {
+    res.write("<h1>Enjoy your weekend</h1>");
+    }  else {
+      res.sendFile(__dirname + "/index.html");
+    }
+
+});
+
+
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
