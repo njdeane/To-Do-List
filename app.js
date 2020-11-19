9f504e64685cd602a413992dd9f5be3285713a6e
njdeane <njdeane7@gmail.com>
2020-11-19 16:02:21 +1000

    MongoDB, Mongoose added for data storage

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const e = require("express");
 
 const app = express();
 
@@ -11,14 +12,40 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useUnifiedTopology: true, useNewUrlParser: true});
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+  const item1 = new Item({
+    name: "Welcome to your todolist!"
+  });
+  
+const item2 = new Item({
+  name: "Hit the + bitton to add a new item."
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err){
+  if(err) {
+    console.log(err);
+  } else {
+    console.log("Successfully updated document.");
+  }
+});
+  
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
